a4fd1cfacced4f9d52954d8a2904c25f4c229fc9
Nate <natepaulo43@gmail.com>
2026-05-13 21:32:11 -0400

    app config

--- misfits/app.config.js
+++ misfits/app.config.js
@@ -41,7 +41,7 @@ module.exports = {
     ios: {
       supportsTablet: false,
       bundleIdentifier: "com.misfits.app",
-      buildNumber: "2",
+      buildNumber: "3",
       googleServicesFile: process.env.GOOGLE_SERVICES_PLIST ?? "./GoogleService-Info.plist",
       infoPlist: {
         // NOTE: Do NOT manually set CFBundleExecutable / CFBundleName /
